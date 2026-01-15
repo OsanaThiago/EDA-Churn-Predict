50b9dbfc164f1de08164e19a0a60c5f17d92eb69
Osana Thiago <osana.thiagosf@gmail.com>
2026-01-15 12:43:56 -0300

    deploy attempt2

--- streamlit/data.py
+++ streamlit/data.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
-from transform.preprocessing import loading_data
-
-df = loading_data()
 
 @st.cache_data
 def load_data():
+    df = pd.read_csv('../data/Customer-Churn-Records.csv')
+    df = df.drop(columns=["RowNumber","CustomerId","Surname",'Complain'])
     return df
 
 @st.cache_data
