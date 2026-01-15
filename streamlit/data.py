e6057e106d60768244767684974dbcc99ee0432c
Osana Thiago <osana.thiagosf@gmail.com>
2026-01-15 13:12:48 -0300

    datapath fix

--- streamlit/data.py
+++ streamlit/data.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv('../data/Customer-Churn-Records.csv')
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    DATA_PATH = BASE_DIR / "data" / "Customer-Churn-Records.csv"
+    df = pd.read_csv(DATA_PATH)
     df = df.drop(columns=["RowNumber","CustomerId","Surname",'Complain'])
     return df
 
